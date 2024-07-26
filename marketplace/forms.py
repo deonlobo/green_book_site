45de7797d8a3c0803854d1082bb4109ced598fdc
sachreetkaur <sachreetkaur112@gmail.com>
2024-07-26 03:06:30 -0400

    fixes

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -3,37 +3,6 @@ from .models import *
 from django_ckeditor_5.widgets import CKEditor5Widget
 
 
-class ProductForm(ModelForm):
-    image_upload = ImageField(required=False, label='Upload Image')
-
-    class Meta:
-        model = Product
-        fields = ['name', 'description', 'price', 'stock', 'image_upload', 'category']
-
-    def save(self, commit=True):
-        product = super().save(commit=False)
-        if self.cleaned_data.get('image_upload'):
-            product.image = self.cleaned_data['image_upload'].read()
-        if commit:
-            product.save()
-        return product
-
-class AddProductForm(ModelForm):
-    image_upload = ImageField(
-        widget=ClearableFileInput(attrs={'class': 'form-control'}),
-        required=False,
-        label='Upload Image'
-    )
-    class Meta:
-        model = Product
-        fields = ['name', 'description', 'price', 'stock','category','image_upload']
-        widgets = {
-            'name': TextInput(attrs={'class': 'form-control'}),
-            'description': Textarea(attrs={'class': 'form-control'}),
-            'price': NumberInput(attrs={'class': 'form-control'}),
-            'stock': NumberInput(attrs={'class': 'form-control'}),
-        }
-
 class ProductStep1Form(ModelForm):
     class Meta:
         model = ProductStep1
@@ -47,6 +16,9 @@ class ProductStep1Form(ModelForm):
             'category': Select(attrs={'class': 'form-control'}),
             'discounted': Select(attrs={'class': 'form-control'}),
             'percent': NumberInput(attrs={'class': 'form-control'}),
+        }
+        labels={
+            'percent': 'Percentage',
         }
         error_messages = {
             'name':{
@@ -84,6 +56,9 @@ class ProductStep1Form(ModelForm):
 
         if stock is not None and stock < 0:
             self.add_error('stock', 'Stock cannot be negative.')
+
+        if discounted is not None and discounted == True and percent is None:
+            self.add_error('percent', 'Please enter percentage for discounted.')
 
         if discounted is not None and discounted == True and percent == 0 :
             self.add_error('percent', 'Percent cannot be zero')
