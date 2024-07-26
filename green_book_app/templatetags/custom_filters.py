8ff3f7376a21ae88002b8c59d7b818cddb7ba038
kashyap-prajapati <prajapatikashyap1997@gmail.com>
2024-07-26 01:33:41 -0400

    Created UI for product listing plage in markteplace

--- green_book_app/templatetags/custom_filters.py
+++ green_book_app/templatetags/custom_filters.py
@@ -2,6 +2,8 @@
 
 from django import template
 from django.utils.timezone import localtime
+from datetime import datetime, timedelta
+from django.utils import timezone
 import pytz
 
 register = template.Library()
@@ -27,3 +29,13 @@ def trim_text(value, length=15):
     if len(value) > length:
         return value[:length] + '...'
     return value
+
+@register.filter
+def is_new_product(created_date):
+
+    if timezone.is_naive(created_date):
+        created_date = timezone.make_aware(created_date, timezone.get_current_timezone())
+
+    now = timezone.now()
+
+    return created_date >= now - timedelta(days=1)
